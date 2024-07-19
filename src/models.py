46b2ea5e68c49395aa913be2bc59b0b77594bbc9
Alexis Rosquete <alexis.rosquete@gmail.com>
2024-07-19 19:30:02 +0000

    first Commit

--- src/models.py
+++ src/models.py
@@ -3,10 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(String(100), nullable=False)
+    last_name = db.Column(String(100), nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +18,16 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
             # do not serialize the password, its a security breach
         }
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False, unique=True)
+    description = Column(String(1000))
+    diameter = Column(Integer, nullable=False)
+    gravity = Column(String(500), nullable=False)
+    climate = Column(String(100), nullable=False)
